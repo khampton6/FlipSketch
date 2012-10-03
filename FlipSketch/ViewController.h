d952f431e1b9da884a847e148a552da2ac4fa588
khampton6 <khampton6@gmail.com>
2012-10-03 17:09:02 -0400

    Adding original xcode project files.

---
+++ FlipSketch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlipSketch
+//
+//  Created by Kevin Hampton on 10/2/12.
+//  Copyright (c) 2012 Kevin Hampton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FlipSketch/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  FlipSketch
+//
+//  Created by Kevin Hampton on 10/2/12.
+//  Copyright (c) 2012 Kevin Hampton. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+  return (interfaceOrientation == UIInterfaceOrientationLandscapeLeft || interfaceOrientation == UIInterfaceOrientationLandscapeRight);
+}
+
+@end
