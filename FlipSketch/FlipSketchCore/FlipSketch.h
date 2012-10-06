260de232a5b653c688c29b18fb488ac21867d80a
khampton6 <khampton6@gmail.com>
2012-10-06 14:50:28 -0400

    Adding Template files.

---
+++ FlipSketch/FlipSketchCore/FlipSketch.h
@@ -0,0 +1,13 @@
+//
+//  FlipSketch.h
+//  FlipSketch
+//
+//  Created by Kevin Hampton on 10/6/12.
+//  Copyright (c) 2012 Kevin Hampton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlipSketch : NSObject
+
+@end
---
+++ FlipSketch/FlipSketchCore/FlipSketch.m
@@ -0,0 +1,13 @@
+//
+//  FlipSketch.m
+//  FlipSketch
+//
+//  Created by Kevin Hampton on 10/6/12.
+//  Copyright (c) 2012 Kevin Hampton. All rights reserved.
+//
+
+#import "FlipSketch.h"
+
+@implementation FlipSketch
+
+@end
