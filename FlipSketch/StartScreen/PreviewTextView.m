18776288491e210187ef3a683a331a6ceacc7220
khampton6 <khampton6@gmail.com>
2012-10-06 14:38:18 -0400

    Added StartScreen element: PreviewTextView

---
+++ FlipSketch/StartScreen/PreviewTextView.m
@@ -0,0 +1,31 @@
+//
+//  PreviewTextView.m
+//  FlipSketch
+//
+//  Created by Kevin Hampton on 10/6/12.
+//  Copyright (c) 2012 Kevin Hampton. All rights reserved.
+//
+
+#import "PreviewTextView.h"
+
+@implementation PreviewTextView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
