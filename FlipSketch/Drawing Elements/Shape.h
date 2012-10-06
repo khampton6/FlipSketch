b01844e63695190b9555606c3a47ec6cb1d5e84c
Brandon <Hozuko@gmail.com>
2012-10-06 14:36:53 -0400

    Added DrawingElements folder and shape class for testing

---
+++ FlipSketch/Drawing Elements/Shape.h
@@ -0,0 +1,13 @@
+//
+//  Shape.h
+//  FlipSketch
+//
+//  Created by Brandon Headrick on 10/6/12.
+//  Copyright (c) 2012 Kevin Hampton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Shape : NSObject
+
+@end
---
+++ FlipSketch/Drawing Elements/Shape.m
@@ -0,0 +1,13 @@
+//
+//  Shape.m
+//  FlipSketch
+//
+//  Created by Brandon Headrick on 10/6/12.
+//  Copyright (c) 2012 Kevin Hampton. All rights reserved.
+//
+
+#import "Shape.h"
+
+@implementation Shape
+
+@end
